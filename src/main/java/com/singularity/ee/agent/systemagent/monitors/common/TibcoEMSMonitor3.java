e9b4a5f6ce0b2b32c17205a60b242e0a389237c1
Todd Radel <tradel@appdynamics.com>
2015-03-16 11:30:11 -0400

    Added the rest of the settings for Tibco SSL

--- src/main/java/com/singularity/ee/agent/systemagent/monitors/common/TibcoEMSMonitor3.java
+++ src/main/java/com/singularity/ee/agent/systemagent/monitors/common/TibcoEMSMonitor3.java
@@ -4,14 +4,16 @@ import com.singularity.ee.agent.systemagent.api.MetricWriter;
 import com.singularity.ee.agent.systemagent.api.TaskExecutionContext;
 import com.singularity.ee.agent.systemagent.api.TaskOutput;
 import com.singularity.ee.agent.systemagent.api.exception.TaskExecutionException;
+import com.tibco.tibjms.TibjmsSSL;
 import com.tibco.tibjms.admin.*;
+import com.tibco.tibjms.admin.ProducerInfo;
+import org.apache.commons.lang.StringUtils;
 
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class TibcoEMSMonitor3 extends JavaServersMonitor
-{
+public class TibcoEMSMonitor3 extends JavaServersMonitor {
     private String tierName;
     private String serverName;
     private String username;
@@ -19,19 +21,26 @@ public class TibcoEMSMonitor3 extends JavaServersMonitor
     private String hostname;
     private String port;
     private String protocol;
+    private String serverUrl;
+    private String sslVendor;
+    private String sslIdentityFile;
+    private String sslIdentityPassword;
+    private String sslTrustedCerts;
+    private String sslIssuerCerts;
+    private boolean sslDebug;
+    private boolean sslVerifyHost;
+    private boolean sslVerifyHostName;
     private List<Pattern> queuePatternsToExclude;
     private List<Pattern> topicPatternsToExclude;
-    private boolean showTempQueues = false;
-    private boolean showSysQueues = false;
+    private boolean showTempQueues;
+    private boolean showSysQueues;
 
 
-    public TibcoEMSMonitor3()
-    {
+    public TibcoEMSMonitor3() {
         oldValueMap = Collections.synchronizedMap(new HashMap<String, String>());
     }
 
-    protected void parseArgs(Map<String, String> args)
-    {
+    protected void parseArgs(Map<String, String> args) {
         super.parseArgs(args);
 
         // if the tier is not specified, then create the metrics for all tiers
@@ -42,6 +51,17 @@ public class TibcoEMSMonitor3 extends JavaServersMonitor
         hostname = getArg(args, "hostname", "localhost");
         port = getArg(args, "port", "7222");
         protocol = getArg(args, "protocol", "tcp");
+
+        sslVendor = getArg(args, "sslVendor", "j2se");
+        sslIdentityFile = getArg(args, "sslIdentityFile", null);
+        sslIdentityPassword = getArg(args, "sslIdentityPassword", null);
+        sslTrustedCerts = getArg(args, "sslTrustedCerts", null);
+        sslIssuerCerts = getArg(args, "sslIssuerCerts", null);
+
+        sslDebug = Boolean.valueOf(getArg(args, "sslDebug", "false"));
+        sslVerifyHost = Boolean.valueOf(getArg(args, "sslVerifyHost", "false"));
+        sslVerifyHostName = Boolean.valueOf(getArg(args, "sslVerifyHostName", "false"));
+
         showTempQueues = Boolean.valueOf(getArg(args, "showTempQueues", "false"));
         showSysQueues = Boolean.valueOf(getArg(args, "showSysQueues", "false"));
 
@@ -49,34 +69,50 @@ public class TibcoEMSMonitor3 extends JavaServersMonitor
         topicPatternsToExclude = parsePatternArg(args, "topicsToExclude");
     }
 
-    private List<Pattern> parsePatternArg(Map<String, String> args, String name)
-    {
+    private List<Pattern> parsePatternArg(Map<String, String> args, String name) {
         ArrayList<Pattern> patternList = new ArrayList<Pattern>();
 
         String[] patterns = getArg(args, name, "").trim().split("\\s+");
-        for (String pattern : patterns)
-        {
+        for (String pattern : patterns) {
             patternList.add(Pattern.compile(pattern));
         }
 
         return patternList;
     }
 
-    private TibjmsAdmin connect() throws TibjmsAdminException
-    {
-        String connectionUrl = String.format("%s://%s:%s", protocol, hostname, port);
+    @SuppressWarnings("unchecked")
+    private TibjmsAdmin connect() throws TibjmsAdminException {
+        Hashtable sslParams = new Hashtable();
 
-        logger.debug(String.format("Connecting to %s as %s", connectionUrl, username));
-        return new TibjmsAdmin(connectionUrl, username, password);
+        if (StringUtils.isNotEmpty(sslIdentityFile)) {
+            sslParams.put(TibjmsSSL.IDENTITY, sslIdentityFile);
+            sslParams.put(TibjmsSSL.PASSWORD, sslIdentityPassword);
+        }
+
+        if (StringUtils.isNotEmpty(sslTrustedCerts)) {
+            sslParams.put(TibjmsSSL.TRUSTED_CERTIFICATES, sslTrustedCerts);
+        }
+
+        if (StringUtils.isNotEmpty(sslIssuerCerts)) {
+            sslParams.put(TibjmsSSL.ISSUER_CERTIFICATES, sslIssuerCerts);
+        }
+
+        sslParams.put(TibjmsSSL.VENDOR, sslVendor);
+        sslParams.put(TibjmsSSL.TRACE, sslDebug);
+        sslParams.put(TibjmsSSL.ENABLE_VERIFY_HOST, sslVerifyHost);
+        sslParams.put(TibjmsSSL.ENABLE_VERIFY_HOST_NAME, sslVerifyHostName);
+
+        serverUrl = String.format("%s://%s:%s", protocol, hostname, port);
+
+        logger.debug(String.format("Connecting to %s as %s", serverUrl, username));
+        return new TibjmsAdmin(serverUrl, username, password, sslParams);
     }
 
-    private void putServerValue(String key, long value)
-    {
+    private void putServerValue(String key, long value) {
         valueMap.put(key, Long.toString(value));
     }
 
-    private void putServerInfo(ServerInfo serverInfo)
-    {
+    private void putServerInfo(ServerInfo serverInfo) {
         putServerValue("DiskReadRate", serverInfo.getDiskReadRate());
         putServerValue("DiskWriteRate", serverInfo.getDiskWriteRate());
 
@@ -100,18 +136,15 @@ public class TibcoEMSMonitor3 extends JavaServersMonitor
         putServerValue("OutboundMessagesPerMinute", getDeltaValue("OutboundMessageCount"));
     }
 
-    private void putDestinationValue(String prefix, String key, long value)
-    {
+    private void putDestinationValue(String prefix, String key, long value) {
         valueMap.put(prefix + "|" + key, Long.toString(value));
     }
 
-    private void putDestinationInfo(String prefix, DestinationInfo destInfo)
-    {
+    private void putDestinationInfo(String prefix, DestinationInfo destInfo) {
         putDestinationValue(prefix, "ConsumerCount", destInfo.getConsumerCount());
         putDestinationValue(prefix, "PendingMessageCount", destInfo.getPendingMessageCount());
-        putDestinationValue(prefix, "FlowControlMaxBytes", destInfo.getFlowControlMaxBytes());
-        putDestinationValue(prefix, "MaxMsgs", destInfo.getMaxMsgs());
         putDestinationValue(prefix, "PendingMessageSize", destInfo.getPendingMessageSize());
+        putDestinationValue(prefix, "FlowControlMaxBytes", destInfo.getFlowControlMaxBytes());
         putDestinationValue(prefix, "MaxMsgs", destInfo.getMaxMsgs());
         putDestinationValue(prefix, "MaxBytes", destInfo.getMaxBytes());
 
@@ -135,8 +168,7 @@ public class TibcoEMSMonitor3 extends JavaServersMonitor
         putDestinationValue(prefix, "OutboundBytesPerMinute", getDeltaValue(prefix + "|OutboundByteCount"));
     }
 
-    private void putQueueInfo(QueueInfo queueInfo)
-    {
+    private void putQueueInfo(QueueInfo queueInfo) {
         String prefix = "Queues|" + queueInfo.getName();
 
         putDestinationInfo(prefix, queueInfo);
@@ -144,8 +176,7 @@ public class TibcoEMSMonitor3 extends JavaServersMonitor
         putDestinationValue(prefix, "ReceiverCount", queueInfo.getReceiverCount());
     }
 
-    private void putTopicInfo(TopicInfo topicInfo)
-    {
+    private void putTopicInfo(TopicInfo topicInfo) {
         String prefix = "Topics|" + topicInfo.getName();
 
         putDestinationInfo(prefix, topicInfo);
@@ -155,21 +186,34 @@ public class TibcoEMSMonitor3 extends JavaServersMonitor
         putDestinationValue(prefix, "DurableCount", topicInfo.getDurableCount());
     }
 
-    private void putQueueInfos(TibjmsAdmin conn) throws TibjmsAdminException
-    {
+    private void putProducerInfos(TibjmsAdmin conn) throws TibjmsAdminException {
+        logger.debug("Retrieving producer information");
+        ProducerInfo[] producerInfos = conn.getProducersStatistics();
+
+        if (producerInfos == null) {
+            logger.warn("Unable to get producer statistics");
+        } else {
+            for (ProducerInfo producerInfo : producerInfos) {
+                logger.info("Publishing metrics for producer " + Long.toString(producerInfo.getID()));
+                putProducerInfo(producerInfo);
+            }
+        }
+
+    }
+
+    private void putProducerInfo(ProducerInfo producerInfo) throws TibjmsAdminException {
+
+    }
+
+    private void putQueueInfos(TibjmsAdmin conn) throws TibjmsAdminException {
         logger.debug("Retrieving queue information");
         QueueInfo[] queueInfos = conn.getQueuesStatistics();
 
-        if (queueInfos == null)
-        {
+        if (queueInfos == null) {
             logger.warn("Unable to get queue statistics");
-        }
-        else
-        {
-            for (QueueInfo queueInfo : queueInfos)
-            {
-                if (shouldMonitorDestination(queueInfo, queuePatternsToExclude))
-                {
+        } else {
+            for (QueueInfo queueInfo : queueInfos) {
+                if (shouldMonitorDestination(queueInfo, queuePatternsToExclude)) {
                     logger.info("Publishing metrics for queue " + queueInfo.getName());
                     putQueueInfo(queueInfo);
                 }
@@ -178,21 +222,15 @@ public class TibcoEMSMonitor3 extends JavaServersMonitor
     }
 
 
-    private void putTopicInfos(TibjmsAdmin conn) throws TibjmsAdminException
-    {
+    private void putTopicInfos(TibjmsAdmin conn) throws TibjmsAdminException {
         logger.debug("Retrieving topic information");
         TopicInfo[] topicInfos = conn.getTopicsStatistics();
 
-        if (topicInfos == null)
-        {
+        if (topicInfos == null) {
             logger.warn("Unable to get topic statistics");
-        }
-        else
-        {
-            for (TopicInfo topicInfo : topicInfos)
-            {
-                if (shouldMonitorDestination(topicInfo, topicPatternsToExclude))
-                {
+        } else {
+            for (TopicInfo topicInfo : topicInfos) {
+                if (shouldMonitorDestination(topicInfo, topicPatternsToExclude)) {
                     logger.info("Publishing metrics for topic " + topicInfo.getName());
                     putTopicInfo(topicInfo);
                 }
@@ -200,27 +238,19 @@ public class TibcoEMSMonitor3 extends JavaServersMonitor
         }
     }
 
-    private boolean shouldMonitorDestination(DestinationInfo destInfo, List<Pattern> patternsToExclude)
-    {
+    private boolean shouldMonitorDestination(DestinationInfo destInfo, List<Pattern> patternsToExclude) {
         String destName = destInfo.getName();
 
-        if (destName.startsWith("$TMP$.") && !showTempQueues)
-        {
+        if (destName.startsWith("$TMP$.") && !showTempQueues) {
             logger.info("Skipping temporary destination '" + destName + "'");
             return false;
-        }
-        else if (destName.startsWith("$sys.") && !showSysQueues)
-        {
+        } else if (destName.startsWith("$sys.") && !showSysQueues) {
             logger.info("Skipping system destination '" + destName + "'");
             return false;
-        }
-        else
-        {
-            for (Pattern patternToExclude : patternsToExclude)
-            {
+        } else {
+            for (Pattern patternToExclude : patternsToExclude) {
                 Matcher matcher = patternToExclude.matcher(destName);
-                if (matcher.matches())
-                {
+                if (matcher.matches()) {
                     logger.info(String.format("Skipping queue '%s' due to excluded pattern '%s'",
                             destName, patternToExclude.pattern()));
                     return false;
@@ -232,34 +262,25 @@ public class TibcoEMSMonitor3 extends JavaServersMonitor
     }
 
     // collects all monitoring data for this time period
-    private void putValuesIntoMap() throws Exception
-    {
+    private void putValuesIntoMap() throws Exception {
         TibjmsAdmin conn = null;
 
-        try
-        {
+        try {
             conn = connect();
 
             ServerInfo serverInfo = conn.getInfo();
             putServerInfo(serverInfo);
 
-//            logger.debug("Retrieving producer information");
-//            ProducerInfo[] producerInfos = conn.getProducersStatistics();
+            logger.debug("Retrieving producer information");
+            ProducerInfo[] producerInfos = conn.getProducersStatistics();
 
             putQueueInfos(conn);
             putTopicInfos(conn);
-        }
-        catch (com.tibco.tibjms.admin.TibjmsAdminException ex)
-        {
-            logger.error("Error connecting to EMS server " + serverName + " "
-                    + port + " " + this.hostname + " " + this.password, ex);
-        }
-        catch (Exception ex)
-        {
+        } catch (com.tibco.tibjms.admin.TibjmsAdminException ex) {
+            logger.error("Error connecting to EMS server " + this.serverUrl, ex);
+        } catch (Exception ex) {
             logger.error("Error getting performance data from Tibco EMS", ex);
-        }
-        finally
-        {
+        } finally {
             if (conn != null) {
                 logger.info("Closing connection to EMS server");
                 conn.close();
@@ -268,33 +289,25 @@ public class TibcoEMSMonitor3 extends JavaServersMonitor
     }
 
     public TaskOutput execute(Map<String, String> taskArguments, TaskExecutionContext taskContext)
-            throws TaskExecutionException
-    {
+            throws TaskExecutionException {
         logger.debug("Starting Execute Thread: " + taskArguments + " : " + taskContext);
 
         startExecute(taskArguments, taskContext);
-        try
-        {
+        try {
             Thread.sleep(5000);
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             logger.error("Sleep was interrupted", e);
         }
 
         // just for debug output
         logger.debug("Starting METRIC COLLECTION for Tibco EMS Monitor");
 
-        try
-        {
+        try {
             putValuesIntoMap();
-            for (Map.Entry<String, String> entry : valueMap.entrySet())
-            {
+            for (Map.Entry<String, String> entry : valueMap.entrySet()) {
                 printMetric(entry.getKey(), entry.getValue());
             }
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             logger.error("Error uploading metrics: " + e.getMessage(), e);
         }
 
@@ -303,31 +316,27 @@ public class TibcoEMSMonitor3 extends JavaServersMonitor
         return this.finishExecute();
     }
 
-    private void printMetric(String name, String value)
-    {
+    private void printMetric(String name, String value) {
         printMetric(name, value,
                 MetricWriter.METRIC_AGGREGATION_TYPE_OBSERVATION,
                 MetricWriter.METRIC_TIME_ROLLUP_TYPE_CURRENT,
                 MetricWriter.METRIC_CLUSTER_ROLLUP_TYPE_COLLECTIVE);
     }
 
-    protected String getMetricPrefix()
-    {
+    protected String getMetricPrefix() {
         logger.debug("tierName=" + tierName + " serverName=" + serverName);
-        if (serverName != null && serverName.trim().length() > 0)
-        {
+        if (serverName != null && serverName.trim().length() > 0) {
             return "Custom Metrics|" + tierName + "|" + serverName + "|";
-        }
-        else
-        {
+        } else {
             return "Custom Metrics|" + tierName + "|";
         }
     }
 
-    public static void main(String[] args) throws TaskExecutionException
-    {
+    public static void main(String[] args) throws TaskExecutionException {
         Map<String, String> params = new HashMap<String, String>();
-        params.put("hostname", "192.168.56.101");
+        params.put("hostname", "localhost");
+        params.put("port", "7243");
+        params.put("protocol", "ssl");
         params.put("password", null);
         params.put("queuesToExclude", "sample");
         new TibcoEMSMonitor3().execute(params, null);
